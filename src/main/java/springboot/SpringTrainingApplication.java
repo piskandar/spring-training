b026962da7a625d456f0bea6c9d778117c47e1fc
Peter Iskandar <bassingitup@gmail.com>
2016-04-10 14:19:58 -0400

    started xml switch

--- src/main/java/springboot/SpringTrainingApplication.java
+++ src/main/java/springboot/SpringTrainingApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("com.acs")
+//@ComponentScan("com.acs")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class SpringTrainingApplication {
 
 	public static void main(String[] args) {
